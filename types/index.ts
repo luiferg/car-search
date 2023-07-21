757b6b3aa642efac42ecd65a43a25161165402f9
Luis Gomez <luisfgomezortiz@gmail.com>
2023-07-21 18:32:14 +0200

    Fixed type error on page.tsx

--- types/index.ts
+++ types/index.ts
@@ -38,6 +38,10 @@ export interface FilterProps {
   model: string
 }
 
+export interface HomeProps {
+  searchParams: FilterProps
+}
+
 export interface OptionProps {
   title: string
   value: string
