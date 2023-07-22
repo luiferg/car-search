f121f156186053191d978a1f034fac7c015b54e3
Luis Gomez <luisfgomezortiz@gmail.com>
2023-07-22 03:13:44 +0200

    fixed bug in SearchManufacturer component for the build

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -59,7 +59,7 @@ const SearchManufacturer = ({
                   value={query}
                   className='search-manufacturer__option'
                 >
-                  Create "{query}"
+                  {`Create "${query}"`}
                 </Combobox.Option>
               ) : (
                 filteredManufacturers.map((item) => (
